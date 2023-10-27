db9faf5c4735f41720dd4667c4aad0c98100242f
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-10-27 08:51:34 -0400

    databased and migrations

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask 
+from flask_migrate import Migrate 
 
 def create_app(): 
     app = Flask(__name__)
 
+    # connect to SQL
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     
+
+    #access to all the built-in SQLAlchemy class methods through models.db
+    from . import models 
+    models.db.init_app(app)    
+    migrate = Migrate(app, models.db)
+
     @app.route('/')
-    def hello(): 
+    def index(): 
         return 'Hello, PetFax!'
 
     # register pet blueprint 
@@ -16,4 +26,3 @@ def create_app():
     app.register_blueprint(fact.bp)
 
     return app
-
